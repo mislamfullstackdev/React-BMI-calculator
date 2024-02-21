bba6d69131de64ed6811828f484b5e590831f1de
mislamfullstackdev <mislam.pasona@gmail.com>
2024-02-21 17:23:58 +0900

    Added BMI status according to result
    Added BMI status according to BMI result.

--- src/App.js
+++ src/App.js
@@ -26,7 +26,35 @@ function App() {
     return (weight / (calculatedHeight * calculatedHeight)).toFixed(1);
   }, [weight, height]);
 
-
+  const bmi_status_calculation = (output) => {
+    let bmi_status = ""
+    let bmi_sign_class = "black"
+    if(output < 18.5){
+      bmi_status = "Underweight"
+      bmi_sign_class = "blue"
+    }
+    else if(output> 18.5 && output<= 24.9){
+      bmi_status = "Normal Weight"
+      bmi_sign_class = "green"
+    }
+    else if(output> 25 && output<= 29.9){
+      bmi_status = "Overweight"
+      bmi_sign_class = "yellow"
+    }
+    else if(output> 30 && output<= 34.9){
+      bmi_status = "Obesity Class I"
+      bmi_sign_class = "red"
+    }
+    else if(output> 35 && output<= 39.9){
+      bmi_status = "	Obesity Class II"
+      bmi_sign_class = "red"
+    }
+    else{
+      bmi_status = "Obesity Class III"
+      bmi_sign_class = "red"
+    }
+    return [bmi_status, bmi_sign_class];
+  } 
   return (
     <main>
       <h1> BMI CALCULATOR </h1>
@@ -53,6 +81,7 @@ function App() {
       <div className="output-section">
         <p> Your BMI </p>
         <p className="output"> {output} </p>
+        <p className="status"><span className={bmi_status_calculation(output)[1]}>〇</span> {bmi_status_calculation(output)[0]} </p>
       </div>
     </main>
   );
